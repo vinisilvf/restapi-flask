9cb2c310b9e11a233adf30ad19f293a6a4791283
Vinícius Ferreira <vinisilv321@gmail.com>
2024-05-12 18:43:19 -0300

    Fix: adding 0.0.0.0 due to connection refused

--- app.py
+++ app.py
@@ -4,19 +4,21 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {'message': 'teste'}
 
+
 class User(Resource):
     def post(self):
         return {'message': 'teste1'}
-    
-    def get(self,cpf):
+
+    def get(self, cpf):
         return {'message': 'pegaocpf'}    
 
 api.add_resource(Users, '/users')
-api.add_resource(User, '/user','/user/<string:cpf>')
+api.add_resource(User, '/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
