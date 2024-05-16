8b8edf7ff21b9d9b382d806c497bb825b378338c
Vinícius Ferreira <vinisilv321@gmail.com>
2024-05-15 22:56:14 -0300

    fix: troubleshooting connection refused 0.0.0.0

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
-from flask import Flask
-from flask_restful import Resource, Api
-
+from flask import Flask, jsonify
+from flask_restful import Resource
+from flask_restful import Api
+from flask_mongoengine import MongoEngine
+ 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'port': 27017,
+    'host': 'mongodb',
+    'username': 'admin',
+    'password': 'admin'
+}
 api = Api(app)
-
+db = MongoEngine(app)
+ 
+ 
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True)
+    birth_date = db.DateTimeField(required=True)
+ 
 
 class Users(Resource):
     def get(self):
-        return {'message': 'teste'}
-
-
+        return jsonify(UserModel.objects())
+        # return {'message': 'user 1'}
+ 
+ 
 class User(Resource):
     def post(self):
-        return {'message': 'teste1'}
-
+        return {'message': 'teste'}
+ 
     def get(self, cpf):
-        return {'message': 'pegaocpf'}    
-
+        return {'message': 'CPF'}
+ 
+ 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
-
+ 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, port=5000 ,host='0.0.0.0')
