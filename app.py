dfc84870859b23ab7f81a505149187b53c459dac
Vinícius Ferreira <vinisilv321@gmail.com>
2024-05-16 00:11:08 -0300

    fix: NotUniqueError and validate CPF

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import re
 from flask import Flask, jsonify
 from flask_restful import Resource
 from flask_restful import Api
 from flask_restful import reqparse
 from flask_mongoengine import MongoEngine
- 
+from mongoengine import NotUniqueError
+
+
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
     'db': 'users',
@@ -65,10 +68,45 @@ class Users(Resource):
  
  
 class User(Resource):
+    
+    def validate_cpf(self, cpf):    
+        # Verifica a formatação do CPF
+        if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
+            return False
+
+        # Obtém apenas os números do CPF, ignorando pontuações
+        numbers = [int(digit) for digit in cpf if digit.isdigit()]
+
+        # Verifica se o CPF possui 11 números ou se todos são iguais:
+        if len(numbers) != 11 or len(set(numbers)) == 1:
+            return False
+
+        # Validação do primeiro dígito verificador:
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:9], range(10, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[9] != expected_digit:
+            return False
+
+        # Validação do segundo dígito verificador:
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[10] != expected_digit:
+            return False
+
+        return True
+
     def post(self):
         data = _user_parser.parse_args()
-        UserModel(**data).save()
- 
+
+        if not self.validate_cpf(data["cpf"]):
+            return {"message": "CPF is invalid!"}, 400
+        
+        try:
+            response = UserModel(**data).save()
+            return {"message": "User %s sucessfully created!" % response.id}
+        except NotUniqueError:
+            return {"message":"CPF already exists in database!"}, 400
+    
     def get(self, cpf):
         return {'message': 'CPF'}
  
